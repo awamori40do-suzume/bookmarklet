d36380b378ced8dbc39623ce4d812711e0a54280
awamori40do-suzume <91420421+awamori40do-suzume@users.noreply.github.com>
2024-09-09 11:14:20 +0900

    Update url.js

--- url.js
+++ url.js
@@ -2,7 +2,7 @@ javascript:(function(){
   var f = 'example.com';  // ドメイン1
   var s = 'anotherdomain.com';  // ドメイン2
   var l = location.href;  // 現在のURL
-  
+
   if (l.indexOf(f) !== -1) {
     // ドメインがfの場合、プロトコルをhttpsにし、ドメインをsに変更
     location.href = l.replace(f, s).replace(/^http:/, 'https:');
